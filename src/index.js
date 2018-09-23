ccae6bf85d4b1e04ea79405e2b95d166a5bfff4c
lVeliarl <dimalitx@gmail.com>
2018-09-23 06:47:16 +0300

    love-triangle/solution: solved the love triangle problem

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 /**
  * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
+ * @returns  number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  let count = 0;
+  for (let i = 0; i < preferences.length; i++)
+    {
+      if (preferences[i] > i + 1)
+      {
+        if (preferences[preferences[preferences[i] - 1] - 1] == i + 1 && preferences[preferences[i] - 1] > i + 1)
+        {
+          count++;
+        }
+      }
+    }
+  return count;
+}
